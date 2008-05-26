dbae46c613d992c5b3f5e82f9acda581d9488f55
tbs.lmaa <tbs.lmaa@a634f1b6-1e4e-0410-8125-33708bab3213>
2008-05-26 12:00:32 +0000

    SessionBeans angelegt

---
+++ ejb3/src/smartPMS/session/LehrangebotSessionBean.java
@@ -0,0 +1,16 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: tbs
+ * Date: 26.05.2008
+ * Time: 13:58:07
+ * To change this template use File | Settings | File Templates.
+ */
+package smartPMS.session;
+
+import javax.ejb.Stateless;
+
+@Stateless(name = "LehrangebotSessionEJB")
+public class LehrangebotSessionBean {
+    public LehrangebotSessionBean() {
+    }
+}
