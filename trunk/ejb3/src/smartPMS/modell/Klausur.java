6f521abda974c011a9b915ae2cc8c3ad2900ec19
tbs.lmaa <tbs.lmaa@a634f1b6-1e4e-0410-8125-33708bab3213>
2008-05-26 11:31:48 +0000

    EJB Annotation eingebaut

--- trunk/ejb3/src/smartPMS/modell/Klausur.java
+++ trunk/ejb3/src/smartPMS/modell/Klausur.java
@@ -1,6 +1,7 @@
 package smartPMS.modell;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,38 +11,82 @@ import javax.persistence.Entity;
  * To change this template use File | Settings | File Templates.
  */
 @Entity
+@Table(name = "Klausur")
 public class Klausur {
 
-    private String datum;
-    private String beginn;
-    private String ende;
+    private long id;
+
+    private Date datum;
+
+    private Date beginn;
+
+    private Date ende;
+
     private double ects = 5.0;
 
+    /**
+     *
+     */
+    protected Klausur() {
+    }
 
-    public String getDatum() {
+
+    /**
+     * @param datum
+     * @param beginn
+     * @param ende
+     * @param ects
+     */
+    public Klausur(Date datum, Date beginn, Date ende, double ects) {
+        this.datum = datum;
+        this.beginn = beginn;
+        this.ende = ende;
+        this.ects = ects;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_klausur_id")
+    @SequenceGenerator(name = "seq_klausur_id", sequenceName = "seq_klausur_id", allocationSize = 1)
+    @Column(name = "id", precision = 10, scale = 0)
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "datum")
+    public Date getDatum() {
         return datum;
     }
 
-    public void setDatum(String datum) {
+    public void setDatum(Date datum) {
         this.datum = datum;
     }
 
-    public String getBeginn() {
+    @Temporal(TemporalType.TIME)
+    @Column(name = "beginn")
+    public Date getBeginn() {
         return beginn;
     }
 
-    public void setBeginn(String beginn) {
+    public void setBeginn(Date beginn) {
         this.beginn = beginn;
     }
 
-    public String getEnde() {
+    @Temporal(TemporalType.TIME)
+    @Column(name = "ende")
+    public Date getEnde() {
         return ende;
     }
 
-    public void setEnde(String ende) {
+    public void setEnde(Date ende) {
         this.ende = ende;
     }
 
+    @Column(name = "ects", precision = 1, scale = 1)
     public double getEcts() {
         return ects;
     }
