00df02ae9d98813c2bf1dff8602b1e753c90089b
tbs.lmaa <tbs.lmaa@a634f1b6-1e4e-0410-8125-33708bab3213>
2008-05-26 13:25:40 +0000

    SessionBeans angelegt

---
+++ web/src/smartPMS/form/PersonListeForm.java
@@ -0,0 +1,11 @@
+package smartPMS.form;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tbs
+ * Date: 26.05.2008
+ * Time: 15:20:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class PersonListeForm {
+}
